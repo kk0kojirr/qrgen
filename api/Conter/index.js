466b12d5e70b85eae152f171b8f69deecc2a998c
kk0kojirro <kk0kojirro>
2020-11-13 04:18:17 +0000

    add api code

--- api/Conter/index.js
+++ api/Conter/index.js
@@ -1,13 +1,52 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+let azure = require('azure-storage');
+const cn = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const tableSvc = azure.createTableService(cn);
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+let updatedTask = {
+    PartitionKey: {'_':'count'},
+    RowKey: {'_': '1'},
+    description: {'_':'update task'},
+    count: {'_': 0}
+};
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+module.exports = function (context, req) {
+    //
+    tableSvc.retrieveEntity('count', 'count', '1', (error, result, response) => {
+        if(!error){
+            const count = result.count._ + 1;
+            updatedTask.count._ = count;
+
+            console.log(count);
+            
+            tableSvc.replaceEntity('count', updatedTask, (error, result, response) => {
+                if(!error) {
+                    // Entity updated
+                    // result contains the entity
+                    const responseMessage = count;
+                    context.res = {
+                        status: 202,
+                        body: {
+                            count: responseMessage
+                        }
+                    };
+                    context.done();
+                } else {
+                    console.log(error);
+                    context.res = {
+                        status: 500,
+                        body: {
+                            error: error
+                        }
+                    }
+                    context.done();
+                }
+            });
+        } else {
+            context.res = {
+                status: 400,
+                body: "Please pass an item in the request body"
+            };
+            context.done();
+        }
+    });
 }
